bdb45bee73d983c7b8e51e5dde050a72c58ccf43
andrmeye <andrmeye@student.arteveldehs.be>
2016-04-16 16:22:01 +0200

    [FEATURE] Laravel Collective

--- www/config/app.php
+++ www/config/app.php
@@ -160,6 +160,7 @@ return [
          * Composer Installed Service Providers...
          */
         Barryvdh\Cors\ServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -205,6 +206,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
 
     ],
 
